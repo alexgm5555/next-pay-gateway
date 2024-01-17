556ec38f76cae75ba15c9746906deb0842761f11
alexgm5555 <alexgm5555@hotmail.com>
2024-01-17 15:16:41 -0500

    fix: deploy

--- app/ui/components/AllProducts/page.tsx
+++ app/ui/components/AllProducts/page.tsx
@@ -2,12 +2,13 @@
 import styles from './page.module.css'
 import { Products } from '../Products/page';
 import { ProductsInterface } from '@/app/lib/data/interfaces';
+import { NextPage } from 'next';
 
 interface props {
   products?: ProductsInterface[];
 }
 
-const AllProducts:React.FC<props> = ({products}) =>{
+const AllProducts:NextPage<props> = ({products}) =>{
 
   return (
     <div className={`${styles['ListProducts-container']}`}>
