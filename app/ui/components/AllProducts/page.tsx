71f29daa4d57602c7a8621943f1f30acec097980
alexgm5555 <alexgm5555@hotmail.com>
2024-01-17 12:28:13 -0500

    fix: netfily

--- app/ui/components/AllProducts/page.tsx
+++ app/ui/components/AllProducts/page.tsx
@@ -1,12 +1,14 @@
-'use client'
+
 import styles from './page.module.css'
 import { Products } from '../Products/page';
 import { ProductsInterface } from '@/app/lib/data/interfaces';
 import { FC } from 'react';
 
-export default async function AllProducts({products}:{
+interface props {
   products?: ProductsInterface[];
-}) {
+}
+
+export const AllProducts:FC<props> = ({products}) =>{
 
   return (
     <div className={`${styles['ListProducts-container']}`}>
